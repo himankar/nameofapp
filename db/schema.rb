88d7407acb0c16640ea1c7eefc5245b50c7f22c3
Himankar Choudhury <himankar.usa@gmail.com>
2017-12-20 19:45:54 -0600

    4.2: fix color - colour

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171220031845) do
+ActiveRecord::Schema.define(version: 20171221013501) do
 
   create_table "products", force: :cascade do |t|
     t.string "name"
@@ -18,7 +18,7 @@ ActiveRecord::Schema.define(version: 20171220031845) do
     t.string "image_url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "color"
+    t.string "colour"
     t.decimal "price"
   end
 
